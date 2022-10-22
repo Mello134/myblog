f032e8583c847ef66520804520a73f46464c42a1
Mello <elproject220@yandex.ru>
2022-10-22 12:55:45 +0300

    Добавили Post модель

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Post #Импортировалм наш Post class. Нашу модель Post
+admin.site.register(Post)
--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+	title =  models.CharField(max_length=300)#поле для ввода строк, максимальное количество символов 300
+	date = models.DateTimeField()#поле дата время
+	text = models.TextField()#для большого тестового поля
+	image = models.ImageField(upload_to='event_images/')#поле ввода для загрузки изображений, upload_to="путь" - место где будет хранится
