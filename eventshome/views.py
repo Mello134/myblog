1b31d7e566acb57433ea6628280e92764639bce2
Mello <elproject220@yandex.ru>
2022-10-31 19:29:41 +0300

    добавили эвенты в home html

--- eventshome/views.py
+++ eventshome/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import Eventhome
 
 # Create your views here.
+def home(request):
+	events = Eventhome.objects
+	return render(request, 'eventshome/home.html', {'events': events})
