41320856f7418a1dadfb654ea0c7fc6cb5977325
Mello <elproject220@yandex.ru>
2022-11-02 19:10:57 +0300

    Добавили страницу главную страницу блога и отдельные страницы блога

--- blog/models.py
+++ blog/models.py
@@ -6,3 +6,6 @@ class Post(models.Model):
 	date = models.DateTimeField()#поле дата время
 	text = models.TextField()#для большого тестового поля
 	image = models.ImageField(upload_to='event_images/')#поле ввода для загрузки изображений, upload_to="путь" - место где будет хранится
+
+	def get_summary(self):#создаём функцию - для получения краткой сводуки
+		return self.text[:70]#вернёт только 70 символов из текста
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404#404 - покажет либо нужный объект, а если его нет покажет 404
+from . models import Post
+
+def showblog(request):#функция показать блог
+	posts = Post.objects
+	return render(request, 'blog/blog.html', {'posts': posts})
+
+def specific_post(request, post_id):#функция отдельного поста
+	post = get_object_or_404(Post, pk=post_id)#Post - класс, pk - основной ключ в базе данных
+	return render(request, 'blog/specific_post.html', {'post':post})
+
+
 
 # Create your views here.
